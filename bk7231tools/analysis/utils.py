f62f3df36ce77788f4586df620e55003eeaa54b2
Khaled Nassar <kmhnassar@gmail.com>
2022-04-09 09:07:44 +0200

    Fix incorrect relative import in analysis/utils

--- bk7231tools/analysis/utils.py
+++ bk7231tools/analysis/utils.py
@@ -1,7 +1,9 @@
-import crc16
 import struct
 
+from .crc16 import crc16
+
+
 def block_crc_check(block: bytes, crc_bytes: bytes) -> bool:
-    calculated = crc16.crc16(block, initial_value=0xffff)
+    calculated = crc16(block, initial_value=0xffff)
     unpacked_crc = (struct.unpack(">H", crc_bytes)[0] & 0xffff)
     return calculated == unpacked_crc
