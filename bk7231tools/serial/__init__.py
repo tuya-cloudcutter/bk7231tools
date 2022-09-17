652bbd5c3260ed9a203222934dea8452477c05e7
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2022-09-17 21:22:06 +0200

    Fix writing 4K-aligned files

--- bk7231tools/serial/__init__.py
+++ bk7231tools/serial/__init__.py
@@ -120,7 +120,8 @@ class BK7231Serial(BK7231CmdFlash):
                 if crc_check:
                     if verbose:
                         print("Verifying CRC")
-                    pad_size = 4096 - (io_size % 4096)
+                    align = io_size % 4096
+                    pad_size = align * (4096 - align)
                     crc = crc32(b"\xff" * pad_size, crc)
                     crc_chip = self.read_flash_range_crc(
                         start=start,
