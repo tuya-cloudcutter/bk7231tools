3f646048853a1e058e8ef42e7c24ce0a3ca9dd56
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2022-11-16 22:29:48 +0100

    Implement decrypting and extracting KV storage partition

---
+++ bk7231tools/analysis/goto.py
@@ -0,0 +1,202 @@
+import array
+import dis
+import functools
+import struct
+import types
+
+
+class _Bytecode:
+    def __init__(self):
+        code = (lambda: x if x else y).__code__.co_code
+        opcode, oparg = struct.unpack_from("BB", code, 2)
+
+        # Starting with Python 3.6, the bytecode format has been changed to use
+        # 16-bit words (8-bit opcode + 8-bit argument) for each instruction,
+        # as opposed to previously 24-bit (8-bit opcode + 16-bit argument) for
+        # instructions that expect an argument or just 8-bit for those that don't.
+        # https://bugs.python.org/issue26647
+        if dis.opname[opcode] == "POP_JUMP_IF_FALSE":
+            self.argument = struct.Struct("B")
+            self.have_argument = 0
+            # As of Python 3.6, jump targets are still addressed by their byte
+            # unit. This, however, is matter to change, so that jump targets,
+            # in the future, will refer to the code unit (address in bytes / 2).
+            # https://bugs.python.org/issue26647
+            self.jump_unit = 8 // oparg
+        else:
+            self.argument = struct.Struct("<H")
+            self.have_argument = dis.HAVE_ARGUMENT
+            self.jump_unit = 1
+
+    @property
+    def argument_bits(self):
+        return self.argument.size * 8
+
+
+_BYTECODE = _Bytecode()
+
+
+def _make_code(code, codestring):
+    args = [
+        code.co_argcount,
+        code.co_nlocals,
+        code.co_stacksize,
+        code.co_flags,
+        codestring,
+        code.co_consts,
+        code.co_names,
+        code.co_varnames,
+        code.co_filename,
+        code.co_name,
+        code.co_firstlineno,
+        code.co_lnotab,
+        code.co_freevars,
+        code.co_cellvars,
+    ]
+
+    try:
+        args.insert(1, code.co_kwonlyargcount)  # PY3
+    except AttributeError:
+        pass
+
+    return code.replace(co_code=codestring)
+    return types.CodeType(*args)
+
+
+def _parse_instructions(code):
+    extended_arg = 0
+    extended_arg_offset = None
+    pos = 0
+
+    while pos < len(code):
+        offset = pos
+        if extended_arg_offset is not None:
+            offset = extended_arg_offset
+
+        opcode = struct.unpack_from("B", code, pos)[0]
+        pos += 1
+
+        oparg = None
+        if opcode >= _BYTECODE.have_argument:
+            oparg = extended_arg | _BYTECODE.argument.unpack_from(code, pos)[0]
+            pos += _BYTECODE.argument.size
+
+            if opcode == dis.EXTENDED_ARG:
+                extended_arg = oparg << _BYTECODE.argument_bits
+                extended_arg_offset = offset
+                continue
+
+        extended_arg = 0
+        extended_arg_offset = None
+        yield (dis.opname[opcode], oparg, offset)
+
+
+def _write_instruction(buf, pos, opname, oparg=0):
+    extended_arg = oparg >> _BYTECODE.argument_bits
+    if extended_arg != 0:
+        pos = _write_instruction(buf, pos, "EXTENDED_ARG", extended_arg)
+        oparg &= (1 << _BYTECODE.argument_bits) - 1
+
+    opcode = dis.opmap[opname]
+    buf[pos] = opcode
+    pos += 1
+
+    if opcode >= _BYTECODE.have_argument:
+        _BYTECODE.argument.pack_into(buf, pos, oparg)
+        pos += _BYTECODE.argument.size
+
+    return pos
+
+
+def _find_labels_and_gotos(code):
+    labels = {}
+    gotos = []
+
+    block_stack = []
+    block_counter = 0
+
+    opname1 = oparg1 = offset1 = None
+    opname2 = oparg2 = offset2 = None
+    opname3 = oparg3 = offset3 = None
+
+    for opname4, oparg4, offset4 in _parse_instructions(code.co_code):
+        if opname1 in ("LOAD_GLOBAL", "LOAD_NAME"):
+            if opname2 == "LOAD_ATTR" and opname3 == "POP_TOP":
+                name = code.co_names[oparg1]
+                if name == "label":
+                    labels[oparg2] = (offset1, offset4, tuple(block_stack))
+                elif name == "goto":
+                    gotos.append((offset1, offset4, oparg2, tuple(block_stack)))
+        elif opname1 in (
+            "SETUP_LOOP",
+            "SETUP_EXCEPT",
+            "SETUP_FINALLY",
+            "SETUP_WITH",
+            "SETUP_ASYNC_WITH",
+        ):
+            block_counter += 1
+            block_stack.append(block_counter)
+        elif opname1 == "POP_BLOCK" and block_stack:
+            block_stack.pop()
+
+        opname1, oparg1, offset1 = opname2, oparg2, offset2
+        opname2, oparg2, offset2 = opname3, oparg3, offset3
+        opname3, oparg3, offset3 = opname4, oparg4, offset4
+
+    return labels, gotos
+
+
+def _inject_nop_sled(buf, pos, end):
+    while pos < end:
+        pos = _write_instruction(buf, pos, "NOP")
+
+
+def _patch_code(code):
+    labels, gotos = _find_labels_and_gotos(code)
+    buf = array.array("B", code.co_code)
+
+    for pos, end, _ in labels.values():
+        _inject_nop_sled(buf, pos, end)
+
+    for pos, end, label, origin_stack in gotos:
+        try:
+            _, target, target_stack = labels[label]
+        except KeyError:
+            raise SyntaxError("Unknown label {0!r}".format(code.co_names[label]))
+
+        target_depth = len(target_stack)
+        if origin_stack[:target_depth] != target_stack:
+            raise SyntaxError("Jump into different block")
+
+        failed = False
+        try:
+            for i in range(len(origin_stack) - target_depth):
+                pos = _write_instruction(buf, pos, "POP_BLOCK")
+            pos = _write_instruction(
+                buf, pos, "JUMP_ABSOLUTE", target // _BYTECODE.jump_unit
+            )
+        except (IndexError, struct.error):
+            failed = True
+
+        if failed or pos > end:
+            raise SyntaxError("Jump out of too many nested blocks")
+
+        _inject_nop_sled(buf, pos, end)
+
+    return _make_code(code, buf.tobytes())
+
+
+def with_goto(func_or_code):
+    if isinstance(func_or_code, types.CodeType):
+        return _patch_code(func_or_code)
+
+    return functools.update_wrapper(
+        types.FunctionType(
+            _patch_code(func_or_code.__code__),
+            func_or_code.__globals__,
+            func_or_code.__name__,
+            func_or_code.__defaults__,
+            func_or_code.__closure__,
+        ),
+        func_or_code,
+    )
---
+++ bk7231tools/analysis/storage.py
@@ -0,0 +1,279 @@
+# Copyright (c) Kuba Szczodrzyński 2022-11-16.
+
+import json
+import os
+from json import JSONDecodeError
+from pathlib import Path
+from struct import unpack
+from typing import List
+
+from Cryptodome.Cipher import AES
+
+from .goto import with_goto
+
+KEY_MASTER = b"qwertyuiopasdfgh"
+# FF's encrypted using master key
+KEY_MAGIC = "46DCED0E672F3B70AE1276A3F8712E03"
+KEY_PART_1 = b"8710_2M"
+KEY_PART_2 = b"HHRRQbyemofrtytf"
+
+# don't complain vscode, please
+goto = None
+label = None
+
+
+def check_crc(crc_read: int, data: bytearray) -> bool:
+    out = 0
+    for b in data:
+        out += b
+    crc_calc = out & 0xFFFFFFFF
+    if crc_read != crc_calc:
+        print(f"\t- invalid CRC: read {crc_read:08x}, calculated {crc_calc:08x}")
+        return False
+    return True
+
+
+def check_magic(expected: int, found: int) -> bool:
+    if expected != found:
+        print(f"\t- invalid magic: expected {expected:08x}, found {found:08x}")
+        return False
+    return True
+
+
+class TuyaStorage:
+    data: bytearray
+    indexes: dict
+
+    def __init__(self, flash_sz: int = 0xE000, swap_flash_sz: int = 0x3000) -> None:
+        self.indexes = {}
+        self.flash_sz = flash_sz
+        self.swap_flash_sz = swap_flash_sz
+        self.block_sz = 1 << 12
+        self.start_addr = 0x200000 - swap_flash_sz - flash_sz
+        self.key_restore_addr = 0x200000 - swap_flash_sz - flash_sz - self.block_sz
+        self.flash_key_addr = self.key_restore_addr
+        self.block_nums = int(flash_sz // self.block_sz)
+        self.swap_block_nums = int(swap_flash_sz // self.block_sz)
+        # not sure what that does
+        v12 = 1
+        while True:
+            self.page_sz = (v12 << 7) & 0xFFFF
+            self.block_pages = int(self.block_sz // self.page_sz) & 0xFFFF
+            v12 *= 2
+            if not (self.block_pages > 8 * ((self.page_sz - 15) & 0xFFFF)):
+                break
+        self.flash_pages = self.block_pages * (self.block_nums & 0xFFFF)
+        self.free_pages = self.flash_pages - self.block_nums
+
+    @property
+    def length(self):
+        return len(self.data)
+
+    def get_output_name(self, key: str, output_directory: str) -> str:
+        dumpfile_name = Path(self.dumpfile).stem
+        if key:
+            return os.path.join(output_directory, f"{dumpfile_name}_storage_{key}.json")
+        return os.path.join(output_directory, f"{dumpfile_name}_storage.json")
+
+    @staticmethod
+    def make_inner_key(inner_key: bytes) -> bytes:
+        key = bytearray(0x10)
+        for i in range(0, 16):
+            key[i] = KEY_PART_1[i & 3] + KEY_PART_2[i]
+        for i in range(16):
+            key[i] = (key[i] + inner_key[i]) % 256
+        return bytes(key)
+
+    def load(self, file: str) -> int:
+        self.dumpfile = file
+        magic = bytes.fromhex(KEY_MAGIC) * 4
+        with open(file, "rb") as f:
+            filedata = f.read()
+        try:
+            pos = filedata.index(magic)
+            pos -= 32  # rewind to block start
+        except ValueError:
+            return None
+        self.data = filedata[pos : pos + self.flash_sz + self.swap_flash_sz]
+        self.data = bytearray(self.data)
+        if len(self.data) != self.flash_sz + self.swap_flash_sz:
+            return None
+        return pos
+
+    def block(self, i: int, new: bytearray = None) -> bytearray:
+        if new:
+            self.data[i * self.block_sz : (i + 1) * self.block_sz] = new
+            return new
+        return self.data[i * self.block_sz : (i + 1) * self.block_sz]
+
+    def page(self, ib: int, ip: int, size: int = 0) -> bytearray:
+        if not size:
+            size = self.page_sz
+        return self.block(ib + 1)[ip * self.page_sz : ip * self.page_sz + size]
+
+    def decrypt(self) -> bool:
+        aes = AES.new(KEY_MASTER, AES.MODE_ECB)
+        master = self.block(0, aes.decrypt(self.block(0)))
+        magic, crc, key = unpack("<II16s", master[0:24])
+        if not check_magic(0x13579753, magic):
+            return False
+        if not check_crc(crc, key):
+            return False
+
+        key = self.make_inner_key(key)
+        aes = AES.new(key, AES.MODE_ECB)
+        for i in range(self.block_nums):
+            block = self.block(i + 1, aes.decrypt(self.block(i + 1)))
+            magic, crc, _ = unpack("<IIH", block[0:10])
+            if not check_magic(0x98761234, magic):
+                return False
+            if not check_crc(crc, block[8:]):
+                return False
+        return True
+
+    def find_all_keys(self) -> List[str]:
+        # go through all elements, trying to find one that doesn't exist
+        self.sf_find_index("abcdef")
+        return list(self.indexes.keys())
+
+    def read_all_keys(self) -> dict:
+        kv = {}
+        for name, index in self.indexes.items():
+            value = self.sf_read_from_index(index)
+            if not value:
+                continue
+            value = value.rstrip(b"\x00").decode()
+            # standard JSON
+            try:
+                kv[name] = json.loads(value)
+                continue
+            except JSONDecodeError:
+                pass
+            # Tuya's weird JSON
+            if name == "user_param_key":
+                value = value[1:-1]
+                value = value.split(",")
+                value = [item.partition(":") for item in value if item]
+                value = {k: int(v) if v.isnumeric() else v for k, _, v in value}
+                value = dict(sorted(value.items()))
+            # else an unknown string
+            kv[name] = value
+        return kv
+
+    def extract_all(self, output_directory: str, separate_keys: bool):
+        kv = self.read_all_keys()
+        out_name = self.get_output_name(None, output_directory)
+        with open(out_name, "w") as f:
+            print(f"\t\textracted all keys to {out_name}")
+            json.dump(kv, f, indent="\t")
+        if not separate_keys:
+            return
+        for key, value in kv.items():
+            out_name = self.get_output_name(key, output_directory)
+            with open(out_name, "w") as f:
+                print(f"\t\textracted '{key}' to {out_name}")
+                json.dump(value, f, indent="\t")
+
+    def sf_read_from_index(self, idx: dict) -> bytearray:
+        buf = bytearray(idx["data_len"])
+        read = 0
+        i = 0
+        while i < idx["element_num"]:
+            element = idx["elements"][i]
+            k = element["start_page_id"]
+            while True:
+                if k > element["end_page_id"]:
+                    break
+                data_len = idx["data_len"]
+                if read + self.page_sz > data_len:
+                    buf[read : read + data_len - read] = self.page(
+                        ib=element["block_id"],
+                        ip=k,
+                        size=data_len - read,
+                    )
+                    read = idx["data_len"]
+                    break
+                buf[read : read + self.page_sz] = self.page(
+                    ib=element["block_id"],
+                    ip=k,
+                )
+                k += 1
+                read += self.page_sz
+            i += 1
+
+        if not check_crc(idx["crc32"], buf[0 : idx["data_len"]]):
+            return None
+        return buf
+
+    @with_goto
+    def sf_find_index(self, name: str):
+        v9 = 0
+        label.label_12
+        if v9 >= self.block_nums:
+            return None
+        v8 = self.page(ib=v9, ip=0)
+        j = 0
+
+        while True:
+            if j >= v8[14]:
+                v9 += 1
+                goto.label_12
+            if v8[j + 15]:
+                break
+            label.label_18
+            j += 1
+
+        k = 0
+        while True:
+            if not ((v8[j + 15] >> k) & 1):
+                goto.label_17
+            v13 = ((8 * j & 0xFF) + k) & 0xFF
+            v14, tmp_index = self.make_sf_index((v8[9] << 8) | v8[8], v13)
+            if not v14:
+                break
+            if v14 != 8:
+                return None
+            label.label_17
+            k += 1
+            if k == 8:
+                goto.label_18
+
+        v15 = len(name)
+        if v15 + 1 != tmp_index["name_len"] or name != tmp_index["name"]:
+            goto.label_17
+
+        return tmp_index
+
+    def make_sf_index(self, block_id: int, page_id: int):
+        v5 = self.page(block_id, page_id, 18)
+        v10 = v5[17] + 18
+        size = v10 + 4 * ((v5[12] << 8) | v5[11])
+        if size > self.page_sz:
+            return 8, None
+        v4 = self.page(block_id, page_id, size)
+        v4 = bytearray(v4)
+        # print(v4.hex(" ", -1))
+        start = v4[17] + 18
+        # v15 = block + i
+        v4s = unpack("<IIHBHIB", v4[0:18])
+        elements = []
+        for i in range(v4s[4]):
+            element = unpack("<HBB", v4[start + i * 4 : start + i * 4 + 4])
+            element = dict(
+                block_id=element[0],
+                start_page_id=element[1],
+                end_page_id=element[2],
+            )
+            elements.append(element)
+        idx = dict(
+            crc32=v4s[0],
+            data_len=v4s[1],
+            block_id=v4s[2],
+            page_id=v4s[3],
+            element_num=v4s[4],
+            elements=elements,
+            name_len=v4s[6],
+            name=v4[18 : 18 + v4s[6]].rstrip(b"\x00").decode(),
+        )
+        self.indexes[idx["name"]] = idx
+        return 0, idx
